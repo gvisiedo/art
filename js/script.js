ae76656a34ec757a012b1263f823fd36a9f2c6a8
gvisiedo <visigoyoweb@gmail.com>
2024-02-23 12:17:47 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 const mainMenu = document.getElementById('nav-principal')
 
 //window.addEventListener('load')
+
+/*
+const menuEmail = document.querySelector('.navbar-email');
+menuEmail.addEventListener('click', toggleDesktopMenu);
+function toggleDesktopMenu(){
+    const isAsideClosed = shoppingCartContainer.classList.contains('inactive');
+    if(!isAsideClosed){
+        shoppingCartContainer.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle('inactive');
+
+}
+
+const productName = document.createElement('p')
+        productName.innerText = '$' + product.name
+
+
+*/
